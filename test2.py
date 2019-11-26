41d7a0fac6b28d905b07ee1b7227a52e7359e91a
LeiZi <1982540042@qq.com>
2019-11-26 17:39:36 +0800

    modify

--- test2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-import datetime
-
-# string = "Thursday, August 29, 2019"
-string = "Thursday, May 2, 2019"
-res = datetime.datetime.strptime(string, "%A, %B %d, %Y")
-print(res, type(res))
